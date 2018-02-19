2e0a2254be386fc24489f19e46d53f93e788235d
deebnntt <daniellebennettchan@gmail.com>
2018-02-19 12:30:20 -0500

    updated api class

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,15 +1,15 @@
-const BASE_URL = 'http://api.giphy.com/v1/gifs/'
-const KEY = 'XfCRmGTeh86OSFFE1WLh1iXicD4KMP5h'
-
 export default class Api {
 
 	static fetchTrendingGifs() {
-		debugger
+		const BASE_URL = 'http://api.giphy.com/v1/gifs/'
+		const KEY = 'XfCRmGTeh86OSFFE1WLh1iXicD4KMP5h'
 		return fetch(`${BASE_URL}trending?api_key=${KEY}&limit=6`)
 			.then(res => res.json())
 	}
 
 	static fetchSearchedGifs(query) {
+		const BASE_URL = 'http://api.giphy.com/v1/gifs/'
+		const KEY = 'XfCRmGTeh86OSFFE1WLh1iXicD4KMP5h'
 		return fetch(`${BASE_URL}search?q=${query}&api_key=${KEY}&limit=9`)
 			.then(res => res.json())
 	}
